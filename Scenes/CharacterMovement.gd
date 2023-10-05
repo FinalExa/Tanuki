b171d5dafb0be7a6dc29d7dd59488d64637e20ef
Alex Silvestri <alexsilvestri.16@gmail.com>
2023-10-05 14:53:39 +0200

    script fix pt2

--- Scenes/CharacterMovement.gd
+++ Scenes/CharacterMovement.gd
@@ -1,13 +1,14 @@
 extends CharacterBody2D
 
 @export var accelerationPerSecond = 0
-@export var maxSpeed = 0
+@export var defaultMaxSpeed = 0
+var currentMaxSpeed
 var currentSpeed
-var currentDirection = Vector2(0,0)
 var inputDirection
 
 func _ready():
 	currentSpeed = 0
+	reset_max_speed()
 
 func get_input():
 	inputDirection = Input.get_vector("left", "right", "up", "down")
@@ -16,8 +17,14 @@ func set_current_speed(delta):
 	if (inputDirection == Vector2.ZERO && currentSpeed!=0):
 		currentSpeed = 0
 	else: if(inputDirection!=Vector2.ZERO):
-		currentSpeed += accelerationPerSecond*delta
+		currentSpeed = clamp(currentSpeed+(accelerationPerSecond*delta),0,currentMaxSpeed)
 	velocity = inputDirection*currentSpeed
+	
+func reset_max_speed():
+	set_max_speed(defaultMaxSpeed)
+	
+func set_max_speed(newMaxSpeed):
+	currentMaxSpeed = newMaxSpeed
 
 func _physics_process(delta):
 	get_input()
