6ec62b64625808dca7fcdde889b8e1e51eddc501
Alex Silvestri <alexsilvestri.16@gmail.com>
2023-10-07 15:02:24 +0200

    Tail added

--- PlayerCharacter/Scripts/TransformationChange.gd
+++ PlayerCharacter/Scripts/TransformationChange.gd
@@ -17,6 +17,8 @@ var isTransformed
 @export var transformationDuration = 0
 @export var tailActivationTime = 0
 @export var timeRefundOnReactivation = 0
+var tailReference
+var tailInstance
 var transformationTimer
 
 @export var transformationLockDuration = 0
@@ -25,6 +27,9 @@ var transformationLock = false
 
 func _ready():
 	transformationTimer = 0
+	tailReference = self.get_child(0)
+	tailInstance = self.get_child(0)
+	self.remove_child(tailInstance)
 
 func _process(delta):
 	set_new_transformation()
@@ -66,14 +71,17 @@ func deactivate_transformation():
 	print("Morbin time over...")
 	emit_signal("reset_speed")
 	isTransformed = false
+	if (self.get_child_count()>0):
+		self.remove_child(tailInstance)
 	transformation_lock_activate()
 
 func transformation_active(delta):
 	if (isTransformed):
 		if (transformationTimer<transformationDuration):
 			transformationTimer=clamp(transformationTimer+delta,0,transformationDuration)
-			#if(transformationTimer>=tailActivationTime):
-				#activate tail
+			if(transformationTimer>=tailActivationTime && self.get_child_count() == 0):
+				add_child(tailReference)
+				tailInstance = self.get_child(0)
 		else:
 			deactivate_transformation()
 	else:
