cd6e04cb1e4b63c33b2a696829928cc4143b2406
Alex Silvestri <alexsilvestri.16@gmail.com>
2023-10-21 20:11:25 +0200

    Local allowed items completed

--- Level/Detection/Scripts/LocalAllowedItems.gd
+++ Level/Detection/Scripts/LocalAllowedItems.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 signal check_if_object
+signal check_if_object_exit
 
 @export var allowedObjects: Array[String]
 @export var assignedObjects: Array[Node2D]
@@ -19,27 +20,37 @@ func _on_body_entered(body):
 func _on_body_exited(body):
 	if (body == playerRef):
 		playerIsIn = false
-		remove_player_from_current_list()
+		remove_item_from_current_list(playerRef.transformationChangeRef)
+	else:
+		emit_signal("check_if_object_exit", body)
 
 func player_inside_area_checks():
 	if (playerIsIn == true):
-		if (playerRef.transformationChangeRef.isTransformed == true && !assignedObjects.has(playerRef.transformationChangeRef)):
-			add_player_to_current_list()
+		if (playerRef.transformationChangeRef.isTransformed == true):
+			add_item_to_current_list(playerRef.transformationChangeRef)
 		else:
-			if (playerRef.transformationChangeRef.isTransformed == false && assignedObjects.has(playerRef.transformationChangeRef)):
-				remove_player_from_current_list()
+			if (playerRef.transformationChangeRef.isTransformed == false):
+				remove_item_from_current_list(playerRef.transformationChangeRef)
 
-func add_player_to_current_list():
-	assignedObjects.push_back(playerRef.transformationChangeRef)
-	print("Added Player")
+func add_item_to_current_list(item: Node2D):
+	if (!assignedObjects.has(item)):
+		assignedObjects.push_back(item)
+		print(str("Added: ", item.name))
 
-func remove_player_from_current_list():
-	assignedObjects.erase(playerRef.transformationChangeRef)
-	print("Removed Player")
+func remove_item_from_current_list(item: Node2D):
+	if (assignedObjects.has(item)):
+		assignedObjects.erase(item)
+		print(str("Removed: ", item.name))
 
 func _on_player_character_give_self_reference(ref):
 	playerRef = ref
 
+func _on_transformation_object_confirm_to_be_sent(receivedObj: Node2D):
+	add_item_to_current_list(receivedObj)
 
-func _on_transformation_object_confirm_to_be_sent(receivedObj):
-	print(receivedObj.name)
+func _on_transformation_object_2_confirm_to_be_sent_exit(receivedObj: Node2D):
+	remove_item_from_current_list(receivedObj)
+
+
+func _on_transformation_object_confirm_to_be_sent_exit():
+	pass # Replace with function body.
--- TransformationObjects/Scripts/TransformationObjectData.gd
+++ TransformationObjects/Scripts/TransformationObjectData.gd
@@ -4,11 +4,11 @@ extends StaticBody2D
 signal set_trs_ready
 signal set_trs_notReady
 signal confirm_to_be_sent
+signal confirm_to_be_sent_exit
 
-@export var transformedName = ""
-@export var transformedMaxSpeed = 0
-@export var transformedProperties = []
-
+@export var transformedName: String
+@export var transformedMaxSpeed: float
+@export var transformedProperties: Array[String]
 
 func _on_trigger_area_set_change_trs_available(status):
 	if (status == true):
@@ -17,6 +17,11 @@ func _on_trigger_area_set_change_trs_available(status):
 		emit_signal("set_trs_notReady")
 
 
-func _on_local_allowed_items_check_if_object(body):
+func _on_local_allowed_items_check_if_object(body: Node2D):
 	if (body == self):
 		emit_signal("confirm_to_be_sent", self)
+
+
+func _on_local_allowed_items_check_if_object_exit(body: Node2D):
+	if (body == self):
+		emit_signal("confirm_to_be_sent_exit", self)
