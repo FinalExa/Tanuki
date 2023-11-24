e91d00f11ee42a783b0f31c73781c89ee464e00a
Alex Silvestri <alexsilvestri.16@gmail.com>
2023-11-24 16:45:06 +0100

    Script roughly done halfway

--- Enemies/Scripts/GuardResearch.gd
+++ Enemies/Scripts/GuardResearch.gd
@@ -13,9 +13,12 @@ var researchEndTimer: float
 var researchTarget: Node2D
 var researchLastPosition: Vector2
 var researchLastDirection: Vector2
+@export var suspiciousItemsThresholdDistance: float
 var suspiciousItemsList: Array[Node2D]
+@export var stunnedGuardsThresholdDistance: float
 var stunnedGuardsList: Array[GuardController]
 var researchHasFoundSomething: bool
+var isDoingResearchAction: bool
 
 @export var guardController: GuardController
 @export var guardAlertValue: GuardAlertValue
@@ -30,7 +33,9 @@ func  _physics_process(delta):
 	research_active(delta)
 
 func initialize_guard_research(target: Node2D):
-	researchTarget = target
+	spotting_operations(target, true)
+	stunnedGuardsList.clear()
+	suspiciousItemsList.clear()
 	guardController.isInResearch = true
 	guardAlertValue.updateText(researchActiveText)
 	research_setup()
@@ -50,24 +55,32 @@ func set_research_target(target: Vector2):
 
 func research_active(delta):
 	if (guardController.isInResearch):
-		researchHasFoundSomething = false
-		var space_state = guardController.get_world_2d().direct_space_state
-		for i in rayTargets.size():
-			var query = PhysicsRayQueryParameters2D.create(guardController.position, rayTargets[i].global_position)
-			var result = space_state.intersect_ray(query)
-			if (result && result != { }):
-				researchHasFoundSomething = spotting_operations(result.collider)
-				if (guardController.isInAlert):
-					return
-		if (researchHasFoundSomething == false):
-			research_end_timer(delta)
-		else:
-			reset_research_end_timer()
+		research_raycasts(delta)
+		priority_actions()
 
-func spotting_operations(trackedObject: Node2D):
-	if ((trackedObject is PlayerCharacter &&
+func research_raycasts(delta):
+	research_main_raycast(delta)
+
+func research_main_raycast(delta):
+	researchHasFoundSomething = false
+	var space_state = guardController.get_world_2d().direct_space_state
+	for i in rayTargets.size():
+		var query = PhysicsRayQueryParameters2D.create(guardController.position, rayTargets[i].global_position)
+		var result = space_state.intersect_ray(query)
+		if (result && result != { }):
+			researchHasFoundSomething = spotting_operations(result.collider, false)
+			if (guardController.isInAlert):
+				return
+	if (researchHasFoundSomething == false && stunnedGuardsList.size() == 0 && suspiciousItemsList.size() == 0):
+		research_end_timer(delta)
+	else:
+		reset_research_end_timer()
+
+func spotting_operations(trackedObject: Node2D, launch: bool):
+	if (!launch &&
+	((trackedObject is PlayerCharacter &&
 	trackedObject.transformationChangeRef.isTransformed == false) ||
-	trackedObject is TailFollow):
+	trackedObject is TailFollow)):
 		stop_research()
 		if (trackedObject is PlayerAttack):
 			guardAlert.start_alert(trackedObject)
@@ -87,6 +100,42 @@ func spotting_operations(trackedObject: Node2D):
 		trackedObject != guardController)):
 			stunnedGuardsList.push_back(trackedObject)
 			return true
+
+func priority_actions():
+	var check: bool = false
+	check = help_guards()
+	if (!check):
+		check = investigate_objects()
+		if (check):
+			return
+
+func help_guards():
+	if (stunnedGuardsList.size()>0):
+		if (researchTarget != stunnedGuardsList[0]):
+			researchTarget = stunnedGuardsList[0]
+			set_research_target(researchTarget.global_position)
+		if (guardController.global_position.distance_to(researchLastPosition) <= stunnedGuardsThresholdDistance):
+			guardMovement.set_location_target(guardController.global_position)
+			stunnedGuardsList[0].guardStunned.end_stun()
+			stunnedGuardsList.remove_at(0)
+		return true
+	return false
+
+func investigate_objects():
+	if (suspiciousItemsList.size()>0):
+		if (researchTarget != suspiciousItemsList[0]):
+			researchTarget = suspiciousItemsList[0]
+			set_research_target(researchTarget.global_position)
+		if (guardController.global_position.distance_to(researchLastPosition) <= suspiciousItemsThresholdDistance):
+			guardMovement.set_location_target(guardController.global_position)
+			if (researchTarget is PlayerCharacter):
+				var tempPlayerReference: PlayerCharacter = researchTarget
+				tempPlayerReference.transformationChangeRef.deactivate_transformation()
+				stop_research()
+				guardAlert.start_alert(tempPlayerReference)
+				suspiciousItemsList.remove_at(0)
+				return true
+	return false
 
 func research_to_check():
 	guardCheck.currentAlertValue = onReturnToCheckAlertValue
