b1a350bbe94d0bf83a486c1f2c85da888c60a84d
Alex Silvestri <alexsilvestri.16@gmail.com>
2024-01-25 18:28:25 +0100

    remove unwanted comment

--- UI/Script/UI.gd
+++ UI/Script/UI.gd
@@ -9,4 +9,4 @@ func _on_transformation_change_send_transformation_name(trsName):
 
 
 func _on_transformation_change_send_transformation_active_info(status, timer, duration):
-	emit_signal("timer_value", status, timer, duration)# Replace with function body.
+	emit_signal("timer_value", status, timer, duration)
