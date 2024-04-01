3a3039ededff32ed41a8ce0506714e4df977ca9e
Alex Silvestri <alexsilvestri.16@gmail.com>
2024-04-01 21:52:28 +0200

    Created AttackHitbox parent class

---
+++ PlayerCharacter/Scripts/PlayerAttackHitbox.gd
@@ -0,0 +1,9 @@
+extends AttackHitbox
+
+func _on_body_entered(body):
+	check_if_enemy(body)
+
+func check_if_enemy(body):
+	if (body is GuardController && !hitTargets.has(body)):
+		body.is_damaged(body.global_position.direction_to(characterRef.global_position))
+		hitTargets.push_back(body)
