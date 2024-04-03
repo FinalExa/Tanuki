185f5a12eb0834c9ca824082ab09b266acbef19d
Alex Silvestri <alexsilvestri.16@gmail.com>
2024-04-03 20:47:31 +0200

    Guard contact now continously rotates with player touching it

--- Enemies/Scripts/GuardContact.gd
+++ Enemies/Scripts/GuardContact.gd
@@ -3,16 +3,30 @@ extends Area2D
 @export var guardController: GuardController
 @export var guardRotator: GuardRotator
 @export var checkValue: float
+var playerIn: bool = false
+var playerSave: Node2D
 
 func _on_body_entered(body):
 	if (body is PlayerCharacter && body.transformationChangeRef.get_if_transformed_in_right_zone() != 1):
+		playerIn = true
+		playerSave = body
+
+func _on_body_exited(body):
+	if (body is PlayerCharacter && playerIn):
+		playerIn = false
+
+func _process(_delta):
+	playerTouching()
+
+func playerTouching():
+	if (playerIn):
 		if (guardController.isInAlert):
-			guardController.guardAlert.lastTargetPosition = body.global_position
-			guardController.guardAlert.extraTargetLocation = body.global_position
+			guardController.guardAlert.lastTargetPosition = playerSave.global_position
+			guardController.guardAlert.extraTargetLocation = playerSave.global_position
 			return
 		if (!guardController.isChecking && guardController.isInPatrol):
 			guardController.guardMovement.set_new_target(null)
 		if (guardController.guardCheck.currentAlertValue < checkValue):
 			guardController.guardCheck.currentAlertValue = checkValue
 		if (!guardController.isStunned):
-			guardRotator.setLookingAtPosition(body.global_position)
+			guardRotator.setLookingAtPosition(playerSave.global_position)
