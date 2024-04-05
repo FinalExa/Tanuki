c1019336c949fc2f6cfa46bbec2bc02fb030a8c4
Alex Silvestri <alexsilvestri.16@gmail.com>
2024-04-05 14:26:17 +0200

    Add guard interaction for ObjectAttack

--- TransformationObjects/Scripts/ObjectAttackHitbox.gd
+++ TransformationObjects/Scripts/ObjectAttackHitbox.gd
@@ -8,6 +8,9 @@ func _on_body_entered(body):
 func check_if_target(body):
 	if (body.is_in_group("Interactable")):
 		body.attackInteraction(attackTag)
+		return
+	if (body is GuardController):
+		body.is_damaged(body.global_position.direction_to(self.global_position))
 
 
 func _on_area_entered(area):
