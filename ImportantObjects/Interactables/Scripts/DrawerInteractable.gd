d1083406fc5716b74b1770d1b5bf2f314110350a
Alex Silvestri <alexsilvestri.16@gmail.com>
2024-04-05 15:55:43 +0200

    Created Crowbar + Playtestmap removed buttons

--- ImportantObjects/Interactables/Scripts/DrawerInteractable.gd
+++ ImportantObjects/Interactables/Scripts/DrawerInteractable.gd
@@ -1,10 +1,14 @@
 extends GenericInteractable
 
+@export var dropActive: bool
 @export var droppedObjectOnInteraction: String
+@export var dropPoint: Node2D
 
 func ExecuteExtraEffect():
-	if (droppedObjectOnInteraction != ""):
+	if (dropActive && droppedObjectOnInteraction != ""):
 		var obj_scene = load(droppedObjectOnInteraction)
 		var obj = obj_scene.instantiate()
 		get_parent().add_child(obj)
-	print("done")
+		for i in get_parent().get_child_count():
+			if (get_parent().get_child(i) == obj):
+				get_parent().get_child(i).global_position = dropPoint.global_position
