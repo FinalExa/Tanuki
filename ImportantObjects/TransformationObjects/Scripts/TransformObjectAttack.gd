2731ce4961d34ba30bba256030c6ddb7b2c15d38
Alex Silvestri <alexsilvestri.16@gmail.com>
2024-04-07 21:34:45 +0200

    Converted weapon tool from time value to frames per second

--- ImportantObjects/TransformationObjects/Scripts/TransformObjectAttack.gd
+++ ImportantObjects/TransformationObjects/Scripts/TransformObjectAttack.gd
@@ -1,5 +1,2 @@
 class_name TransformObjectAttack
 extends ExecuteAttack
-
-func _process(delta):
-	attacking(delta)
--- PlayerCharacter/Scripts/PlayerAttack.gd
+++ PlayerCharacter/Scripts/PlayerAttack.gd
@@ -1,9 +1,8 @@
 class_name PlayerAttack
 extends ExecuteAttack
 
-func _process(delta):
+func _process(_delta):
 	check_for_attack_input()
-	attacking(delta)
 
 func check_for_attack_input():
 	if (!attackLaunched && Input.is_action_just_pressed("attack") && !characterRef.transformationChangeRef.isTransformed):
