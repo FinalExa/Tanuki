acf2f3c3d1a839cb4498eb0e7f73473a4b569aef
Alex Silvestri <alexsilvestri.16@gmail.com>
2024-06-07 18:28:15 +0200

    Fix to hyottoko enemy

--- Enemies/Scripts/BehaviorTree/TaskHyottokoApproachPlayer.gd
+++ Enemies/Scripts/BehaviorTree/TaskHyottokoApproachPlayer.gd
@@ -4,6 +4,7 @@ func Evaluate(_delta):
 	var dist = enemyController.global_position.distance_to(enemyController.playerRef.global_position)
 	if (dist > enemyController.hyottokoPushDistanceFromPlayer):
 		if (enemyController.enemyMovement.target != enemyController.playerRef):
+			enemyController.enemyMovement.reset_movement_speed()
 			enemyController.enemyMovement.set_new_target(enemyController.playerRef)
 			enemyController.enemyRotator.setLookingAtNode(enemyController.playerRef)
 		return NodeState.FAILURE
--- Enemies/Scripts/BehaviorTree/TaskIsSpottingPlayer.gd
+++ Enemies/Scripts/BehaviorTree/TaskIsSpottingPlayer.gd
@@ -3,6 +3,4 @@ extends GuardNode
 func Evaluate(_delta):
 	if (enemyController.isSpottingPlayer):
 		return NodeState.FAILURE
-	if (!enemyController.isInPatrol):
-		enemyController.enemyPatrol.resume_patrol()
 	return NodeState.SUCCESS
