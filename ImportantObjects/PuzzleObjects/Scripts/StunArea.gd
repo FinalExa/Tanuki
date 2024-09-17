1243e78a056cf88b2ee0c3765008b6d3de88e243
Alex Silvestri <alexsilvestri.16@gmail.com>
2024-09-17 16:19:34 +0200

    Implemented stunarea only stunning at the start

--- ImportantObjects/PuzzleObjects/Scripts/StunArea.gd
+++ ImportantObjects/PuzzleObjects/Scripts/StunArea.gd
@@ -1,26 +1,42 @@
 extends PuzzleObject
 
 @export var attackTag: String
+@export var stunDuration: float
+var stunTimer: float
+var stunActive: bool
+
+func _process(delta):
+	StunTimer(delta)
+
+func StunTimer(delta):
+	if (stunActive):
+		if (stunTimer > 0):
+			stunTimer -= delta
+			return
+		stunActive = false
 
 func Activation():
 	if (get_parent() == null):
-		call_deferred("AddChild")
+		call_deferred("Initialize")
 
 func Deactivation():
-	call_deferred("RemoveChild")
+	call_deferred("TurnOff")
 
-func AddChild():
+func Initialize():
 	parentRef.add_child(self)
+	stunActive = true
+	stunTimer = stunDuration
 
-func RemoveChild():
+func TurnOff():
 	parentRef.remove_child(self)
 
 func _on_body_entered(body):
-	if (body.is_in_group("Interactable")):
-		body.AttackInteraction(attackTag)
-		return
-	if (body is EnemyController):
-		body.is_damaged(Vector2.ZERO)
+	if (stunActive):
+		if (body.is_in_group("Interactable")):
+			body.AttackInteraction(attackTag)
+			return
+		if (body is EnemyController):
+			body.is_damaged(Vector2.ZERO)
 
 func _on_area_entered(area):
 	if (area.is_in_group("Interactable")):
