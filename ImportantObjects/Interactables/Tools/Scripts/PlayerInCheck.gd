ec5298393b2bf7208185b8a6c9b5ba6a672b380c
Alex Silvestri <alexsilvestri.16@gmail.com>
2024-11-15 14:30:13 +0100

    You can now drop in the secret room from the roof without a softlock

---
+++ ImportantObjects/Interactables/Tools/Scripts/PlayerInCheck.gd
@@ -0,0 +1,8 @@
+class_name PlayerInCheck
+extends Area2D
+
+@export var connectedInteractable: GenericInteractable
+
+func _on_body_entered(body):
+	if (body is PlayerCharacter):
+		connectedInteractable.FinalState()
