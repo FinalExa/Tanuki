5a1f213dee9c7abefe51b807879ed9076eea13de
Alex Silvestri <alexsilvestri.16@gmail.com>
2024-11-19 15:10:54 +0100

    Fixed crash

--- ImportantObjects/Interactables/Tools/Scripts/PlayerInCheck.gd
+++ ImportantObjects/Interactables/Tools/Scripts/PlayerInCheck.gd
@@ -5,4 +5,4 @@ extends Area2D
 
 func _on_body_entered(body):
 	if (body is PlayerCharacter):
-		connectedInteractable.FinalState()
+		if (connectedInteractable != null): connectedInteractable.FinalState()
