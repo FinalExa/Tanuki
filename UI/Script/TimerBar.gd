1aa6777722ae3d21a6f8440be6d4e847081e6ead
Alex Silvestri <alexsilvestri.16@gmail.com>
2024-11-27 17:08:53 +0100

    Bar has progression colors now

--- UI/Script/TimerBar.gd
+++ UI/Script/TimerBar.gd
@@ -2,8 +2,21 @@ class_name TimerBar
 extends TextureProgressBar
 
 @export var multiplyOffset: float
+@export var startColor: Color
+@export var midColor: Color
+@export var endColor: Color
 
 func UpdateTimer(timer, duration):
 	self.max_value = duration * multiplyOffset
 	timer = abs(duration - timer)
+	UpdateColor(timer, duration)
 	self.value = timer * multiplyOffset
+
+func UpdateColor(timer, duration):
+	if (timer >= duration - (duration / 3)):
+		tint_progress = startColor
+	else:
+		if (timer >= duration - ((duration / 3) * 2)):
+			tint_progress = midColor
+		else:
+			tint_progress = endColor
