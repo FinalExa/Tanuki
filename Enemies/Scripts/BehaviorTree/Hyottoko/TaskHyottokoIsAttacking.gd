e24085f37fc370b982ed5318a1cf871b125f1149
Alex Silvestri <alexsilvestri.16@gmail.com>
2024-12-17 17:12:40 +0100

    Hyottoko Cleanup 2 + remove old code, rework complete

--- Enemies/Scripts/BehaviorTree/Hyottoko/TaskHyottokoIsAttacking.gd
+++ Enemies/Scripts/BehaviorTree/Hyottoko/TaskHyottokoIsAttacking.gd
@@ -1,10 +1,9 @@
 extends GuardNode
 
-@export var enemyAttackNormal: EnemyAttack
-@export var enemyAttackRage: EnemyAttack
+@export var enemyAttack: EnemyAttack
 
 func Evaluate(_delta):
-	if (enemyAttackNormal.attackLaunched || enemyAttackRage.attackLaunched):
+	if (enemyAttack.attackLaunched):
 		if(enemyController.enemyMovement.locationTargetEnabled || enemyController.enemyMovement.target != null):
 			enemyController.enemyMovement.set_new_target(null)
 		return NodeState.FAILURE
