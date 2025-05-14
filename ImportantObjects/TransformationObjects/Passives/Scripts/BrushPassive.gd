289f52dba5d17fb29a13082716ec8f2e48a47fbb
Alex Silvestri <alexsilvestri.16@gmail.com>
2025-05-14 16:48:58 +0200

    Added proper color change

--- ImportantObjects/TransformationObjects/Passives/Scripts/BrushPassive.gd
+++ ImportantObjects/TransformationObjects/Passives/Scripts/BrushPassive.gd
@@ -14,14 +14,23 @@ enum BrushColor
 @export var spriteRef: Sprite2D
 @export var colors: Array[Color]
 @export var tags: Array[String]
+@export var UIDescription: Array[String]
 var currentColorYokaiIn: ColorYokai
 var currentColor: BrushColor
 
 func _process(delta):
 	if (!transformationChangeRef.isTransformed):
+		if (self.visible): self.hide()
 		ChangeColor(defaultColor)
 	else:
+		if (!self.visible): self.show()
+		FollowBrushSprite()
 		if (currentColorYokaiIn != null): ChangeColor(currentColorYokaiIn.colorOfYokai)
+
+func FollowBrushSprite():
+	spriteRef.global_position = transformationChangeRef.transformationSprite.global_position
+	spriteRef.rotation_degrees = transformationChangeRef.transformationSprite.rotation_degrees
+	spriteRef.flip_h = transformationChangeRef.transformationSprite.flip_h
 
 func ChangeColor(colorToSet: BrushColor):
 	if (colorToSet != currentColor):
@@ -32,7 +41,6 @@ func ChangeColor(colorToSet: BrushColor):
 func _on_new_color_check_body_entered(body):
 	if (body is ColorYokai):
 		currentColorYokaiIn = body
-
 
 func _on_new_color_check_body_exited(body):
 	if (body is ColorYokai && body == currentColorYokaiIn):
