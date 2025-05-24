8b937ef871cda4201b325c4acaae719e322dcbe1
Alex Silvestri <alexsilvestri.16@gmail.com>
2025-05-24 15:03:17 +0200

    Created items for chef quest

--- ImportantObjects/Interactables/Scripts/ChefYokaiInteractable.gd
+++ ImportantObjects/Interactables/Scripts/ChefYokaiInteractable.gd
@@ -1,1 +1,18 @@
+class_name ChefYokaiInteractable
 extends GenericInteractable
+
+@export var acceptedColdFoods: Array[String]
+@export var coldFoodSprites: Array[Texture]
+@export var acceptedHotFoods: Array[String]
+@export var hotFoodSprites: Array[Texture]
+@export var coldFoodRequest: Sprite2D
+@export var hotFoodRequest: Sprite2D
+var selectedColdFood: String
+var selectedHotFood: String
+
+func ReadyOperations():
+	SelectFoods()
+
+func SelectFoods():
+	selectedColdFood = acceptedColdFoods[randi_range(0, acceptedColdFoods.size() - 1)]
+	selectedHotFood = acceptedHotFoods[randi_range(0, acceptedHotFoods.size() - 1)]
