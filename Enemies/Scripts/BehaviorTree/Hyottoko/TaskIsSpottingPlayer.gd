e7f24cf8583c4295951f2674b056e1e400d3615c
Alex Silvestri <alexsilvestri.16@gmail.com>
2025-08-08 15:22:27 +0200

    Way better hyottoko tracking

--- Enemies/Scripts/BehaviorTree/Hyottoko/TaskIsSpottingPlayer.gd
+++ Enemies/Scripts/BehaviorTree/Hyottoko/TaskIsSpottingPlayer.gd
@@ -5,4 +5,6 @@ func Evaluate(_delta):
 		if (enemyController.isInPatrol):
 			enemyController.enemyPatrol.stop_patrol()
 		return NodeState.FAILURE
+	if (!enemyController.isInPatrol):
+		enemyController.enemyPatrol.resume_patrol()
 	return NodeState.SUCCESS
--- Enemies/Scripts/Hyottoko/HyottokoRange.gd
+++ Enemies/Scripts/Hyottoko/HyottokoRange.gd
@@ -2,60 +2,70 @@ class_name HyottokoRange
 extends Area2D
 
 @export var hyottokoController: HyottokoController
-@export var collisionShape:  CollisionShape2D
+@export var collisionShape: CollisionShape2D
 @export var baseRadius: float
 @export var extendedRadius: float
+@export var timeBeforeUnset: float
+var unsetTimer: float
+var unsetTimerActive: bool
 var target: PlayerCharacter
-var raycastActive: bool
 var playerSpotStatus: int
 
 func _ready():
 	playerSpotStatus = -1
 	collisionShape.shape.radius = baseRadius
 
-func _physics_process(_delta):
+func _process(delta):
+	UnsetTimer(delta)
+
+func _physics_process(delta):
 	RangeRaycast()
 
 func RangeRaycast():
-	if (raycastActive && !hyottokoController.isStunned && !hyottokoController.isReachingPoint):
+	if (target != null && !hyottokoController.isStunned && !hyottokoController.isReachingPoint):
 		var space_state = hyottokoController.get_world_2d().direct_space_state
 		var query = PhysicsRayQueryParameters2D.create(hyottokoController.global_position, target.global_position)
-		query.exclude = [hyottokoController, hyottokoController.hyottokoAttack]
+		query.exclude = [hyottokoController, collisionShape, hyottokoController.hyottokoAttack]
 		var result = space_state.intersect_ray(query)
 		if (result && result != { }):
+			print(result.collider.name)
 			if (result.collider is PlayerCharacter):
 				if (!CheckPlayerTransformationStatus(result.collider)):
 					SetSpottingPlayer()
 					return
-		UnsetSpottingPlayer()
+		if (!unsetTimerActive):
+			unsetTimerActive = true
 
 func _on_body_entered(body):
 	if (body is PlayerCharacter):
 		target = body
-		raycastActive = true
 
 func _on_body_exited(body):
 	if (body is PlayerCharacter):
 		target = null
-		raycastActive = false
-		UnsetSpottingPlayer()
+		unsetTimerActive = true
 
 func SetSpottingPlayer():
 	if (!hyottokoController.isSpottingPlayer):
 		playerSpotStatus = 0
 		hyottokoController.isSpottingPlayer = true
 		collisionShape.shape.radius = extendedRadius
-		hyottokoController.enemyPatrol.stop_patrol()
+		unsetTimer = timeBeforeUnset
+		unsetTimerActive = false
 		if (hyottokoController.playerRef == null):
 			hyottokoController.playerRef = target
+
+func UnsetTimer(delta):
+	if (unsetTimerActive):
+		if (unsetTimer > 0):
+			unsetTimer -= delta
+			return
+		UnsetSpottingPlayer()
 
 func CheckPlayerTransformationStatus(playerRef: PlayerCharacter):
 	var playerStatus: int = playerRef.transformationChangeRef.get_if_transformed_in_right_zone()
 	if (playerSpotStatus == -1):
 		playerSpotStatus = playerStatus
-	if (target.velocity != Vector2.ZERO):
-		playerSpotStatus = 0
-		return false
 	if (playerSpotStatus == 1 && playerStatus == 1):
 		return true
 	if (playerSpotStatus != playerStatus):
@@ -65,6 +75,7 @@ func CheckPlayerTransformationStatus(playerRef: PlayerCharacter):
 func UnsetSpottingPlayer():
 	if (hyottokoController.isSpottingPlayer):
 		hyottokoController.isSpottingPlayer = false
+		unsetTimerActive = false
 		playerSpotStatus = -1
 		collisionShape.shape.radius = baseRadius
-		hyottokoController.enemyPatrol.resume_patrol()
+		unsetTimer = 0
