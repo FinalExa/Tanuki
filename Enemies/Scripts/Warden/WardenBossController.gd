b44e75f713d13ac0c3f9fd4e324e158b893a9f57
Alex Silvestri <alexsilvestri.16@gmail.com>
2025-09-05 15:58:00 +0200

    Added boss fight second room

--- Enemies/Scripts/Warden/WardenBossController.gd
+++ Enemies/Scripts/Warden/WardenBossController.gd
@@ -2,12 +2,17 @@ class_name WardenBossController
 extends WardenController
 
 @export var newPatrol: PatrolIndicator
+@export var newAlertArea: WardenAlertArea
 var changedPatrol: bool = false
 
 func AdvanceBossPhase():
 	if (!changedPatrol):
 		patrolIndicators.clear()
 		patrolIndicators.push_back(newPatrol)
+		wardenAlertArea.queue_free()
+		wardenAlertArea = newAlertArea
+		wardenCheck.wardenAlertArea = newAlertArea
+		wardenAlertArea.wardenCheck = wardenCheck
 		enemyPatrol.loadedPatrolIndicator = patrolIndicators[0]
 		enemyPatrol.reset_patrol()
 		enemyPatrol.restart_patrol()
