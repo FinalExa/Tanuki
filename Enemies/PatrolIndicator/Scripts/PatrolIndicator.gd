387b915e3465efbf6c4592bd2b44292411f21435
Alex Silvestri <alexsilvestri.16@gmail.com>
2025-11-20 14:57:17 +0100

    Removed all old patrols, updated kitchen patrols with new method

--- Enemies/PatrolIndicator/Scripts/PatrolIndicator.gd
+++ Enemies/PatrolIndicator/Scripts/PatrolIndicator.gd
@@ -15,7 +15,3 @@ enum LookDirections {
 }
 
 @export var patrolIndexes: Array[PatrolIndex]
-@export var patrolActions: Array[ActionTypes]
-@export var moveActions: Array[Node2D]
-@export var waitActions: Array[float]
-@export var lookActions: Array[LookDirections]
